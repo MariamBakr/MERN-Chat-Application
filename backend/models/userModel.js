39b75025414052624738d317f3fb8048d8cc75c1
MariamBakr <mariam.bakrgaafer@gmail.com>
2023-04-24 02:59:02 +0200

    first commiy

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose')
 const userSchema=mongoose.Schema({
     name:{type:String,required:true},
-    picture:{type:String,required:true,default:'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg'},
-    email:{type:String,required:true},
+    picture:{type:String,default:'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg'},
+    email:{type:String,required:true,unique:true},
     password:{type:String,required:true}
 },{timestamps:true})
 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require ('express')
 const chats=require('./data/data')
 const dotenv=require('dotenv')
+const connectDB = require('./config/db')
+const userRoutes= require('./routes/userRoutes')
+
 const app = express()
 dotenv.config()
+connectDB()
 app.get('/',(req,res)=>{
     res.send("API is running...")
 })
@@ -13,5 +17,7 @@ app.get('/api/chat/:id',(req,res)=>{
     const singleChat=chats.find((c)=>c._id===req.params.id)
     res.send(singleChat)
 })
+
+app.use('/api/user',userRoutes)
 const PORT=process.env.PORT||5000
 app.listen(PORT,console.log(`server running on port ${PORT}...`))
